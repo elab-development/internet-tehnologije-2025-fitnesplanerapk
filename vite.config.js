2250147b6fb53f95b82d1ee1892a1582b36e8cc3
TinaV3 <vidakovich03@gmail.com>
2026-03-09 02:27:42 +0100

    putanje izmenjene za dockerfile i vite6

--- vite.config.js
+++ vite.config.js
@@ -2,24 +2,26 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'path';
 
 export default defineConfig({
     plugins: [
         laravel({
-            // 1. Ovde kažeš Laravelu gde je React ulazni fajl
-            input: ['react/src/main.jsx'], 
+            // Dodajemo ./ ispred da Vite zna da je to lokalni FOLDER, a ne biblioteka
+            input: ['./react/src/main.jsx'], 
             refresh: true,
-            // 2. Ovde kažeš Laravelu gde će biti manifest.json
-            buildDirectory: 'build', 
         }),
         react(),
         tailwindcss(),
     ],
+    resolve: {
+        alias: {
+            // Ovo eksplicitno govori Vite-u gde se nalazi tvoj izvorni kod
+            '@': resolve(__dirname, './react/src'),
+        },
+    },
     build: {
-        // 3. Ovo osigurava da build ide u public/build, a ne u dist
         outDir: 'public/build',
         emptyOutDir: true,
-        // 4. VAŽNO: Ovo forsira da manifest.json ne ode u .vite podfolder
-        manifest: 'manifest.json', 
     }
 });
