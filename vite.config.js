f39b2f29df588619306dac7d5c568fead26d7a6d
TinaV3 <vidakovich03@gmail.com>
2026-03-09 01:47:40 +0100

    putanje izmenjene za dockerfile i vite2

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,15 @@
-import { defineConfig } from 'vite'
-import laravel from 'laravel-vite-plugin'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
 export default defineConfig({
     plugins: [
         laravel({
             input: ['react/src/main.jsx'],
             refresh: true,
-            // Ovo osigurava da manifest bude u public/build
-            buildDirectory: 'build', 
+            buildDirectory: 'build', // Ovo je ključ za Laravel
         }),
         react(),
         tailwindcss(),
     ],
     build: {
-        // Ovo će naterati Vite da izbaci fajlove tamo gde Laravel očekuje
-        outDir: '../public/build', 
+        outDir: 'dist', // Neka ostane 'dist'
         emptyOutDir: true,
     }
 })
